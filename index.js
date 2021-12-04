28a55867681205657ae20685ff51ea24b05aded2
Jack Keller <jack.keller@wwt.com>
2021-12-04 15:23:46 -0600

    refactored em to rem for styles, some reformatting markup and syncing css/html

--- index.js
+++ index.js
@@ -7,27 +7,25 @@ function render(resume) {
   var template = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
 	var partialsDir = path.join(__dirname, 'partials');
   
-	// var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-  
   var filenames = fs.readdirSync(partialsDir);
   filenames.forEach(function (filename) {
-	  var matches = /^([^.]+).hbs$/.exec(filename);
+    var matches = /^([^.]+).hbs$/.exec(filename);
 	  if (!matches) {
-	    return;
+      return;
 	  }
 	  var name = matches[1];
 	  var filepath = path.join(partialsDir, filename)
 	  var template = fs.readFileSync(filepath, 'utf8');
-
+    
 	  Handlebars.registerPartial(name, template);
 	});
-
-
+  
+  
 	// Nicer dates
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 	Handlebars.registerHelper('date', function(date) {
-	  var theDate = new Date(date);
-	  return months[theDate.getMonth()] + ' ' + theDate.getFullYear();
+    const theDate = new Date(date);
+	  return `${months[theDate.getMonth()]} ${theDate.getFullYear()}`;
 	});
 
 	return Handlebars.compile(template)({
